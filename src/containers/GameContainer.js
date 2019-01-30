715259167906407a21bb4ad7993f8bc5acb6c331
Steven Kay <stevendkay@gmail.com>
2019-01-30 22:48:37 +0000

    style changes

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -14,7 +14,19 @@ class GameContainer extends Component {
       winnerNumber: 0, // 0 = neither, 1 = O, 2 = X
       winLine: [] // 3 indexes to winning line's squares
     };
-    this.winLines = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
+    this.winLines = [
+      //rows
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // columns
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // diagonals
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
     this.handleSquareClick = this.handleSquareClick.bind(this);
     this.handleResetClick = this.handleResetClick.bind(this);
   };
@@ -64,7 +76,7 @@ class GameContainer extends Component {
   }
 
   handleResetClick() {
-    // rest the board
+    // reset the board
     this.setState({
       squareStates : [' ',' ',' ',' ',' ',' ',' ',' ',' '],
       currentPlayer : 1, // 1 (O) or 2 (X)
