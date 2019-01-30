456b571b9fd67bb64f653ebbbf80f8e8e848cd64
Christina Davies <christinahdavies@gmail.com>
2019-01-30 12:04:22 +0000

    basic css applied

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,11 +6,11 @@ class Board extends Component {
     let squaresArray = [];
     for(let squareIndex = 0; squareIndex < 9 ; squareIndex += 1){
       let squareSymbol = this.props.squareStates[squareIndex];
-      let square = <div>I am square {squareIndex} and my value is {squareSymbol}</div>;
+      let square = <div className="square">I am square {squareIndex} and my value is {squareSymbol}</div>;
       squaresArray.push(square);
     }
     return (
-      <div className="square">{squaresArray}</div>
+      <div className="squares">{squaresArray}</div>
     )
   }
 
